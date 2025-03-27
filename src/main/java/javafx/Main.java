a0c736e72a686c75827123db66834e433c6e47aa
NCV17 <nguyencongvuk17@gmail.com>
2025-03-27 17:58:25 +0700

    Mô tả thay đổi

--- src/main/java/javafx/Main.java
+++ src/main/java/javafx/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
             e.printStackTrace();
         }
         // Mở cửa sổ với kích thước tối đa mà vẫn giữ lại thanh tiêu đề của hệ điều hành
-        stage.setMaximized(true);
+       // stage.setMaximized(true);
     }
     public static void main(String[] args) {
         launch(args); // Gọi launch() trong Main để bắt đầu ứng dụng
